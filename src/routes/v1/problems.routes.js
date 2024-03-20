d8bc83af0f37e6bbcd9e2468b460c17cf119ceaa
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-03-20 22:12:10 +0530

    Added routing structure for versioned api for problems

---
+++ src/routes/v1/problems.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { ProblemController } = require('../../controllers/index');
+
+const problemRouter = express.Router();
+
+problemRouter.get('/ping', ProblemController.pingProblemController);
+problemRouter.get('/:id', ProblemController.getProblem);
+problemRouter.get('/', ProblemController.getProblems);
+problemRouter.post('/', ProblemController.addProblem);
+problemRouter.delete('/:id', ProblemController.deleteProblem);
+problemRouter.patch('/:id', ProblemController.updateProblem);
+
+module.exports = problemRouter;
